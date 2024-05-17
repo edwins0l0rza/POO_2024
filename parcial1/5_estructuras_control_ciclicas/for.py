dadfa53428a1789c849bd6934a7551e78ebb14ac
EDWIN ALFREDO SOLORZA OCHOA <edwinsolorza3@gmail.com>
2024-05-17 11:36:07 -0600

    for

--- parcial1/5_estructuras_control_ciclicas/for.py
+++ parcial1/5_estructuras_control_ciclicas/for.py
@@ -1,19 +1,18 @@
 # Ciclo For Estructura iterativa que se ejecuta X veces
 
 # Sintaxis
-#     for variable in elemento_iterable(lista, rango, etc)
+#     for variable in elemento_iterable(lista, rango, etc):
 #         bloque de instrucciones
 
 # Ejemplo 1 Crear un program que imprima en pantalla 5 veces el @
 
-# contador=1
+
 
 # for contador in range(1,6):
 #     print("@")
 
 #Ejemplo 2 Crear un programa que imprima los numeros del 1 al 5 y los sume y al final imprima la suma
 
-# contador=1
 # suma=0
 # for contador in range(1,6):
 #     print(contador)
@@ -26,7 +25,6 @@
 
 tabla=int(input("Ingresa un numero para calcular su tabla de multiplicar: "))
 
-i=1
 multi=0
 
 for i in range (1,11):
