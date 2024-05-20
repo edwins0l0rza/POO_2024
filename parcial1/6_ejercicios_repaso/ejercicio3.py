b0c7d5e09ae9fdd9e1ce2b4092dc258bf554bc7c
EDWIN ALFREDO SOLORZA OCHOA <edwinsolorza3@gmail.com>
2024-05-20 08:49:35 -0600

    ejercicio3

---
+++ parcial1/6_ejercicios_repaso/ejercicio3.py
@@ -0,0 +1,14 @@
+#3.- Escribir un programa que muestre los cuadrados 
+#(un numero multiplicado por si mismo) de los 60 primeros 
+#numeros naturales. Resolverlo con while y for
+
+respuesta="si"
+while respuesta == "si":
+ print("Ingrese un nuemro del 0 al 59: ")
+ numero = int(input(""))
+
+ cuadrado = numero**2
+
+ print("El cuadrado de su numero es: ")
+ print(cuadrado)
+ respuesta=input("¿Desea ingresar otro numero?")
