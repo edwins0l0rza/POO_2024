1423cfaf14241414a560cc802f68d08c1d0578b5
EDWIN ALFREDO SOLORZA OCHOA <edwinsolorza3@gmail.com>
2024-05-20 08:49:15 -0600

    ejercicio1

---
+++ parcial1/6_ejercicios_repaso/ejercicio1.py
@@ -0,0 +1,13 @@
+#1.- Crear 2 variables una pais y otra continente 
+# Mostrar su valor en pantalla
+#Poner un comentario para poner el tipo de dato
+
+pais="MÉXICO"
+continente="AMÉRICA"
+
+print(pais)
+print(continente)
+
+print(input("Presione enter para saber el tipo de dato"))
+print(type(pais))
+print(type(continente))
