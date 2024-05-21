5986f54254c11ef82ce7c71e586f0229bcc2f68b
EDWIN ALFREDO SOLORZA OCHOA <edwinsolorza3@gmail.com>
2024-05-20 18:59:12 -0600

    ejercicio4

---
+++ parcial1/6_ejercicios_repaso/ejercicio4.py
@@ -0,0 +1,14 @@
+#4.- Solicitar 2 numeros al usuario
+# y realizar todas las operaciones
+# basicas de una calculadora (+,-,*,/)
+# y mostrar por pantalla el resultado
+
+a = int(input('Digita el primer numero: '))
+b = int(input('Digita el segundo numero: '))
+
+suma=a+b
+resta=a-b
+multiplicacion=a*b
+division=a/b
+
+print(f'Suma: {suma}\nResta: {resta}\nMultiplicacion: {multiplicacion}\nDivision: {division}')
