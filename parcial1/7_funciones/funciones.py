f19de63444557cff83d3f21a59cd8579d8d022f7
EDWIN ALFREDO SOLORZA OCHOA <edwinsolorza3@gmail.com>
2024-05-30 09:45:44 -0600

    fuciones

--- parcial1/7_funciones/funciones.py
+++ parcial1/7_funciones/funciones.py
@@ -20,3 +20,37 @@ Sintaxis:
   3.- Funcion que recibe parametros y no regresa valor
   4.- Funcion que recibe parametros y regresa valor
 """
+
+#1.- Funcion que no recibe parametros y no regresa valor
+"""def sumaNumeros1():
+    n1=int(input("Numero #1: "))
+    n2=int(input("Numero #2: "))
+    suma=n1+n2
+    print(f"{n1}+{n2}={suma}")
+
+
+sumaNumeros1()"""
+#2.- Funcion que no recibe parametros y regresa valor
+"""def sumaNumeros2():
+    n1=int(input("Numero #1: "))
+    n2=int(input("Numero #2: "))
+    suma=n1+n2
+    return f"{n1}+{n2}={suma}"
+
+print(sumaNumeros2())"""
+#3.- Funcion que recibe parametros y no regresa valor
+"""def sumaNumeros3(n1,n2):
+    suma=n1+n2
+    print(f"{n1}+{n2}={suma}")
+
+n1=int(input("Numero #1:"))
+n2=int(input("Numero #2:"))
+sumaNumeros3(n1,n2)"""
+#4.- Funcion que recibe parametros y regresa valor
+def sumaNumeros4(n1,n2):
+    suma=n1+n2
+    return f"{n1}+{n2}={suma}"
+
+n1=int(input("Numero #1:"))
+n2=int(input("Numero #2:"))
+print(sumaNumeros4(n1,n2))
