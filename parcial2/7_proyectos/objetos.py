071332d64365e53c1eff7bcb4b8f7a0395f30893
EDWIN ALFREDO SOLORZA OCHOA <edwinsolorza3@gmail.com>
2024-07-12 11:09:34 -0600

    ER Parcial3 CRUD

---
+++ parcial2/7_proyectos/objetos.py
@@ -0,0 +1,11 @@
+from lectores import *
+
+
+estudiante1=Estudiantes("Ana Torres Guzman", "Col. Centro 1500 ote", "6181002614", "MECA", "21651523")
+
+docente1=Docentes("Daniel Fuentes Loera", "Fracc. D. Arrieta 1400 nte", "6181445152", "TI", "234")
+
+
+estudiante1.reservar()
+docente1.reservar()
+
